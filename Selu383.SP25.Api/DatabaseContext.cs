ce103c25e537f8fb1c4d620f6b7975e0331086c0
Jake Saurage <jake.saurage03@gmail.com>
2025-02-05 16:21:42 -0600

    Database Connect
    Please name your database SP25-P01-G10. Installed packages and created a migrations folder

---
+++ Selu383.SP25.Api/DatabaseContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Selu383.SP25.Api.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+    }
+}
--- Selu383.SP25.Api/Program.cs
+++ Selu383.SP25.Api/Program.cs
@@ -1,19 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Selu383.SP25.Api.Data; // Ensure this matches your namespace
 
 namespace Selu383.SP25.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            // Add Database Context
+            builder.Services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             var app = builder.Build();
+
+            // Apply migrations at startup
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+                await db.Database.MigrateAsync();
+            }
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -22,10 +35,7 @@ namespace Selu383.SP25.Api
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
